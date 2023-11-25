7a458e44da7f081c7beb06b5b73e237b2b69b33f
logunlaja26 <logunlaja26@gmail.com>
2023-11-24 20:03:01 -0600

    Added jackson to session model for data-bind for incoming json request

--- src/main/java/com/bnkrll/model/Session.java
+++ src/main/java/com/bnkrll/model/Session.java
@@ -1,5 +1,6 @@
 package com.bnkrll.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,18 +8,25 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Session {
-    private String sessionId;
+    private String sessionId = UUID.randomUUID().toString();
+    @JsonProperty("paytype")
     private PayType payType;
+    @JsonProperty("gametype")
     private GameType gameType;
+    @JsonProperty("limittype")
     private LimitType limitType;
+    @JsonProperty("profit")
     private BigDecimal gameProfit;
+    @JsonProperty("location")
     private String location;
+    @JsonProperty("date")
     private LocalDate date;
     private BigDecimal totalAfterProfit;
 
