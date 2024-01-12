5d9293a965c7ce0955a18061723b6d402d7d701b
logunlaja26 <logunlaja26@gmail.com>
2024-01-12 15:47:09 -0600

    COnfigurations for AWS Serverless Lambda

--- src/main/java/com/bnkrll/controller/SessionController.java
+++ src/main/java/com/bnkrll/controller/SessionController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/session")
 @Slf4j
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173, http://127.0.0.1:3000" )
 public class SessionController {
 
     private final SessionRepository sessionRepository;
---
+++ src/main/java/com/bnkrll/handler/AsynchronousLambdaHandler.java
@@ -0,0 +1,27 @@
+package com.bnkrll.handler;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.bnkrll.BnkrllServiceApplication;
+
+public class AsynchronousLambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+    private SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    public AsynchronousLambdaHandler() throws ContainerInitializationException {
+        handler = new SpringBootProxyHandlerBuilder<AwsProxyRequest>()
+                .defaultProxy()
+                .asyncInit()
+                .springBootApplication(BnkrllServiceApplication.class)
+                .buildAndInitialize();
+    }
+
+    @Override
+    public AwsProxyResponse handleRequest(AwsProxyRequest input, Context context) {
+        return handler.proxy(input, context);
+    }
+}
